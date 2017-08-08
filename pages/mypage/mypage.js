8f3d4ead5257e33b9c4a4042a8de7e5e3cdfe217
Vantza <544082780@qq.com>
2017-08-08 11:47:17 +0800

    增加商铺图片显示
    1、优化图片排列显示，更加美观；
    2、增加图片点击事件；

--- pages/mypage/mypage.js
+++ pages/mypage/mypage.js
@@ -1,4 +1,25 @@
 // pages/mypage/mypage.js
+var imageArray = [{
+  mode: 'scaleToFill',
+  src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168332830&di=54eb0868f8b40826d91261de52d6bbb2&imgtype=0&src=http%3A%2F%2Fwww.goumin.com%2Fattachments%2Fphoto%2F0%2F0%2F66%2F17125%2F4384100o2.jpg'
+}, {
+  mode: 'aspectFit',
+  src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168332830&di=ab116ec2ad012592e734fc43d5121b80&imgtype=0&src=http%3A%2F%2Fimg1.blog.eastmoney.com%2Fcr%2Fcrystalran%2F201405%2F20140509224746534.jpg'
+}, {
+  mode: 'top',
+  src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168527477&di=ee771fed0eeaab1ab2ab4a132cff99f5&imgtype=jpg&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D29373325%2C2147126213%26fm%3D214%26gp%3D0.jpg'
+}, {
+  mode: 'scaleToFill',
+  src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168527477&di=ee771fed0eeaab1ab2ab4a132cff99f5&imgtype=jpg&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D29373325%2C2147126213%26fm%3D214%26gp%3D0.jpg'
+}];
+
+var clickImageArray = [
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168332830&di=54eb0868f8b40826d91261de52d6bbb2&imgtype=0&src=http%3A%2F%2Fwww.goumin.com%2Fattachments%2Fphoto%2F0%2F0%2F66%2F17125%2F4384100o2.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168332830&di=ab116ec2ad012592e734fc43d5121b80&imgtype=0&src=http%3A%2F%2Fimg1.blog.eastmoney.com%2Fcr%2Fcrystalran%2F201405%2F20140509224746534.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168527477&di=ee771fed0eeaab1ab2ab4a132cff99f5&imgtype=jpg&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D29373325%2C2147126213%26fm%3D214%26gp%3D0.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502168527477&di=ee771fed0eeaab1ab2ab4a132cff99f5&imgtype=jpg&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D29373325%2C2147126213%26fm%3D214%26gp%3D0.jpg'
+];
+
 
 Page({
 
@@ -10,16 +31,7 @@ Page({
     businessHours: '09:00-17:00',
     phoneNumText: '18621924350,(0513)85197186',
     picText: '门店照片',
-    imageArray: [{
-      mode: 'scaleToFill',
-      src: '../../resources/myimages/logo.jpg'
-    },{
-      mode: 'aspectFit',
-      src: '../../resources/myimages/logo.jpg'
-    }, {
-      mode: 'top',
-      src: '../../resources/myimages/logo.jpg'
-    }]
+    imageArray: imageArray
   },
 
   makeCalling: function () {
@@ -46,6 +58,21 @@ Page({
       },
       fail: function () {
         console.log("打开地图失败！")
+      }
+    })
+  },
+
+  previewlmg: function (event) {
+    var imgIdx = event.currentTarget.dataset.imgIdx;
+    wx.previewImage({
+      current: clickImageArray[imgIdx],
+      urls: clickImageArray,
+      success: function () {
+        console.log("打开图片成功！")
+        console.log(imgIdx)
+      },
+      fail: function () {
+        console.log("打开图片失败！")
       }
     })
   },
